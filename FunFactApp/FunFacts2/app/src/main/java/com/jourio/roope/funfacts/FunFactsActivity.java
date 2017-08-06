dba994cc5b7c0ea95027a3f40dd97ffe2b7f422d
Roope Jourio <jourioroope@gmail.com>
2017-08-07 01:00:26 +0300

    couple changes.

--- FunFactApp/FunFacts2/app/src/main/java/com/jourio/roope/funfacts/FunFactsActivity.java
+++ FunFactApp/FunFacts2/app/src/main/java/com/jourio/roope/funfacts/FunFactsActivity.java
@@ -2,12 +2,27 @@ package com.jourio.roope.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    // Declare our View Variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Assign the Views from the layout file to the corresponding variables.
+        mFactTextView = (TextView) findViewById(R.id.factTextView):
+
     }
+
+
+
 }

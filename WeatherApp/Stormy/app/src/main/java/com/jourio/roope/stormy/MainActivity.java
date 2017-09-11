107c69b987480285c77178ec45ef77d79bfa33ff
Roope Jourio <jourioroope@gmail.com>
2017-09-11 12:19:36 +0300

    Making HTTP GET request with OkHttp

--- WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/MainActivity.java
+++ WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/MainActivity.java
@@ -2,12 +2,58 @@ package com.jourio.roope.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "92404b5cbe52d93eb96035b97f166d07";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "/" + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+

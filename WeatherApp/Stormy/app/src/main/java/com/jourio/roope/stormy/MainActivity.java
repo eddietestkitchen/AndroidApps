166db323daf2da82eb331f4dbdaeb58c4044fcbd
Roope Jourio <jourioroope@gmail.com>
2017-09-11 12:37:31 +0300

    Small fix with regex.

--- WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/MainActivity.java
+++ WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         double latitude = 37.8267;
         double longitude = -122.4233;
         String forecastUrl = "https://api.darksky.net/forecast/" + apiKey +
-                "/" + latitude + "/" + longitude;
+                "/" + latitude + "," + longitude;
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
@@ -32,14 +33,23 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
 
 
     }

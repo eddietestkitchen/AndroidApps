aec11127da7a3bb23b24a29bd6b91153ee6a0360
Roope Jourio <jourioroope@gmail.com>
2017-09-19 20:52:16 +0300

    getting daily and hourly weather data now from the darksky API

--- WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/ui/MainActivity.java
+++ WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/ui/MainActivity.java
@@ -165,15 +165,51 @@ public class MainActivity extends AppCompatActivity {
         return forecast;
     }
 
-    private Day[] getDailyForecast(String jsonData) {
-        return new Day[0];
+    private Day[] getDailyForecast(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject daily = forecast.getJSONObject("daily");
+        JSONArray data = daily.getJSONArray("data");
+
+        Day[] days = new Day[data.length()];
+
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject jsonDay = data.getJSONObject(i);
+            Day day = new Day();
+
+            day.setSummary(jsonDay.getString("summary"));
+            day.setIcon(jsonDay.getString("icon"));
+            day.setTemperatureMax(jsonDay.getDouble("temperatureMax"));
+            day.setTime(jsonDay.getLong("time"));
+            day.setTimezone(timezone);
+
+            days[i] = day;
+        }
+
+        return days;
     }
 
     private Hour[] getHourlyForecast(String jsonData) throws JSONException {
         JSONObject forecast = new JSONObject(jsonData);
         String timezone = forecast.getString("timezone");
-        JSONObject hourly = forecast.getJSONObject("hourly") // self explanatory
+        JSONObject hourly = forecast.getJSONObject("hourly"); // self explanatory
         JSONArray data = hourly.getJSONArray("data");
+
+        Hour[] hours = new Hour[data.length()];
+
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject jsonHour = data.getJSONObject(i);
+            Hour hour = new Hour();
+
+            hour.setSummary(jsonHour.getString("summary"));
+            hour.setTemperature(jsonHour.getDouble("temperature"));
+            hour.setIcon(jsonHour.getString("icon"));
+            hour.setTime(jsonHour.getLong("time"));
+            hour.setTimezone(timezone);
+
+            hours[i] = hour;
+        }
+        return hours;
     }
 
     private Current getCurrentDetails(String jsonData) throws JSONException {

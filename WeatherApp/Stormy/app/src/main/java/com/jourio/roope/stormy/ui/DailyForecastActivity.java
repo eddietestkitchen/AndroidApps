da95b60e57776e13bf19050e3dd0d186e9b51120
Roope Jourio <jourioroope@gmail.com>
2017-09-20 05:23:10 +0300

    daily forecast activity now displays days in the dayOfTheWeek Array

--- WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/ui/DailyForecastActivity.java
+++ WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/ui/DailyForecastActivity.java
@@ -3,6 +3,7 @@ package com.jourio.roope.stormy.ui;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.jourio.roope.stormy.R;
 
@@ -14,5 +15,9 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                daysOfTheWeek);
+        setListAdapter(adapter);
     }
 }

65581fb547b163bdc92a0c107ba1d2e28e3e03f8
Roope Jourio <jourioroope@gmail.com>
2017-09-21 08:16:50 +0300

    making just-in-time getView method for the days.

--- WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/adapters/DayAdapter.java
+++ WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package com.jourio.roope.stormy.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.jourio.roope.stormy.R;
 import com.jourio.roope.stormy.weather.Day;
 
 public class DayAdapter extends BaseAdapter {
@@ -28,12 +32,33 @@ public class DayAdapter extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int i) {
+    public long getItemId(int position) {
         return 0; // we aren't going to use this. Tag items for easy reference.
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            // brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
         return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView; // public by default
+        TextView temperatureLabel;
+        TextView dayLabel;
     }
 }

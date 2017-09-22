c6ff2c9d9ccf8aa6e19ab4988c1a5f32e6821953
Roope Jourio <jourioroope@gmail.com>
2017-09-23 01:42:52 +0300

    New activity with recyclerView, wiring the hourly button.

---
+++ WeatherApp/Stormy/app/src/main/java/com/jourio/roope/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,15 @@
+package com.jourio.roope.stormy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jourio.roope.stormy.R;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+    }
+}

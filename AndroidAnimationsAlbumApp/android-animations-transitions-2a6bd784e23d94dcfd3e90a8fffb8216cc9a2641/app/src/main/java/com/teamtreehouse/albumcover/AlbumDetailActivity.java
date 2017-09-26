45eb35633b91b5c63ab37e2c1ae46c711f3a6041
Roope Jourio <jourioroope@gmail.com>
2017-09-26 10:44:12 +0300

    fab button animation from 0 to 1.

--- AndroidAnimationsAlbumApp/android-animations-transitions-2a6bd784e23d94dcfd3e90a8fffb8216cc9a2641/app/src/main/java/com/teamtreehouse/albumcover/AlbumDetailActivity.java
+++ AndroidAnimationsAlbumApp/android-animations-transitions-2a6bd784e23d94dcfd3e90a8fffb8216cc9a2641/app/src/main/java/com/teamtreehouse/albumcover/AlbumDetailActivity.java
@@ -34,8 +34,18 @@ public class AlbumDetailActivity extends Activity {
         populate();
     }
 
+    private void animate() {
+        fab.setScaleX(0); // Initial value set at 0.
+        fab.setScaleY(0);
+        fab.animate()
+                .scaleX(1)
+                .scaleY(1)
+                .start();
+    }
+
     @OnClick(R.id.album_art)
     public void onAlbumArtClick(View view) {
+        animate();
 
     }
 

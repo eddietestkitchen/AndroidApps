4c0960504504c3d479b6c93aef44903761cb6fef
Roope Jourio <jourioroope@gmail.com>
2017-10-13 11:59:04 +0300

    A lot of refactoring..

--- SmellsLikeBakin/SmellsLikeBakin/app/src/main/java/com/example/roope/smellslikebakin/CheckBoxesFragment.java
+++ SmellsLikeBakin/SmellsLikeBakin/app/src/main/java/com/example/roope/smellslikebakin/CheckBoxesFragment.java
@@ -1,8 +1,5 @@
 package com.example.roope.smellslikebakin;
-
-
 import android.support.v4.app.Fragment;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,7 +8,8 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
-public class DirectionsFragment extends Fragment{
+
+public class CheckBoxesFragment extends Fragment {
     private static final String KEY_CHECKED_BOXES = "key_checked_boxes";
     private CheckBox[] mCheckBoxes;
 
@@ -19,28 +17,33 @@ public class DirectionsFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         int index = getArguments().getInt(ViewPagerFragment.KEY_RECIPE_INDEX);
-        View view = inflater.inflate(R.layout.fragment_directions, container, false);
+        boolean isIngredients = getArguments().getBoolean(ViewPagerFragment.KEY_IS_INGREDIENTS);
+        View view = inflater.inflate(R.layout.fragment_checkboxes, container, false);
 
-        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.directionsLayout);
-        String[] directions = Recipes.directions[index].split("`"); // splits the words in to array, so the directions can be checkboxed.
-
-        mCheckBoxes = new CheckBox[directions.length];
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.checkBoxesLayout);
+        String[] contents;
+        if (isIngredients) {
+            contents = Recipes.ingredients[index].split("`");
+        } else {
+            contents = Recipes.directions[index].split("`");
+        }
+        mCheckBoxes = new CheckBox[contents.length];
         boolean[] checkedBoxes = new boolean[mCheckBoxes.length];
         if (savedInstanceState != null && savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES) != null) {
             checkedBoxes = savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES);
         }
-        setUpCheckBoxes(directions, linearLayout, checkedBoxes);
+        setUpCheckBoxes(contents, linearLayout, checkedBoxes);
 
         return view;
     }
 
-    private void setUpCheckBoxes(String[] directions, ViewGroup container, boolean[] checkedBoxes) {
+    private void setUpCheckBoxes(String[] contents, ViewGroup container, boolean[] checkedBoxes) {
         int i = 0;
-        for (String direction : directions) {
+        for (String content : contents) {
             mCheckBoxes[i] = new CheckBox(getActivity());
             mCheckBoxes[i].setPadding(8, 16, 8, 16);
             mCheckBoxes[i].setTextSize(20f);
-            mCheckBoxes[i].setText(direction);
+            mCheckBoxes[i].setText(content);
             container.addView(mCheckBoxes[i]);
             if (checkedBoxes[i]) { // If checkedBoxes should be checked
                 mCheckBoxes[i].toggle(); // finally saving and restoring state of our check boxes.

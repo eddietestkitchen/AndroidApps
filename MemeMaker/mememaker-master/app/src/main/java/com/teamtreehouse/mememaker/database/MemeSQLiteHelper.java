e3c44af03a8303ef7e2d002573526a2a21eaa32d
Roope Jourio <jourioroope@gmail.com>
2017-10-26 00:52:13 +0300

    Setting up SQLite DB

--- MemeMaker/mememaker-master/app/src/main/java/com/teamtreehouse/mememaker/database/MemeSQLiteHelper.java
+++ MemeMaker/mememaker-master/app/src/main/java/com/teamtreehouse/mememaker/database/MemeSQLiteHelper.java
@@ -5,10 +5,34 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
-public class MemeSQLiteHelper {
+public class MemeSQLiteHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "memes.db";
+    public static final int DB_VERSION = 1;
     //Meme Table functionality
+    public static final String MEMES_TABLE = "MEMES";
+    public static final String COLUMN_MEME_ASSET = "ASSET";
+    public static final String COLUMN_MEME_NAME = "NAME";
+    private static String CREATE_MEMES =
+            "CREATE TABLE " + MEMES_TABLE + "("
+                    + BaseColumns._ID
+                    + "  INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_MEME_ASSET + " TEXT," +
+                    COLUMN_MEME_NAME + " TEXT)";
 
     //Meme Table Annotations functionality
+    
+    public MemeSQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_MEMES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }

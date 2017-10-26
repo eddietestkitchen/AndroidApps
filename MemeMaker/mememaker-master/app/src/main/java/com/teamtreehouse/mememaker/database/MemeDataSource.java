7edcef584583cb4e8ff816282e9eac262e335c3c
Roope Jourio <jourioroope@gmail.com>
2017-10-26 10:19:27 +0300

    Creating new data for the MEMES table, using ContentValues Class

--- MemeMaker/mememaker-master/app/src/main/java/com/teamtreehouse/mememaker/database/MemeDataSource.java
+++ MemeMaker/mememaker-master/app/src/main/java/com/teamtreehouse/mememaker/database/MemeDataSource.java
@@ -34,7 +34,23 @@ public class MemeDataSource {
     public void create(Meme meme) {
         SQLiteDatabase database = open();
         database.beginTransaction();
-        //implementation details
+
+        ContentValues memeValues = new ContentValues();
+        memeValues.put(MemeSQLiteHelper.COLUMN_MEME_NAME, meme.getName());
+        memeValues.put(MemeSQLiteHelper.COLUMN_MEME_ASSET, meme.getAssetLocation());
+        long memeID = database.insert(MemeSQLiteHelper.MEMES_TABLE, null, memeValues); // second argument only needed when trying to insert a row without any value.
+
+        for (MemeAnnotation annotation : meme.getAnnotations()) {
+            ContentValues annotationValues = new ContentValues();
+            annotationValues.put(MemeSQLiteHelper.COLUMN_ANNOTATION_COLOR, annotation.getColor());
+            annotationValues.put(MemeSQLiteHelper.COLUMN_ANNOTATION_TITLE, annotation.getTitle());
+            annotationValues.put(MemeSQLiteHelper.COLUMN_ANNOTATION_X, annotation.getLocationX());
+            annotationValues.put(MemeSQLiteHelper.COLUMN_ANNOTATION_Y, annotation.getLocationY());
+            annotationValues.put(MemeSQLiteHelper.COLUMN_FOREIGN_KEY_MEME, memeID);
+
+            database.insert(MemeSQLiteHelper.ANNOTATIONS_TABLE, null, annotationValues);
+        }
+
         database.setTransactionSuccessful();
         database.endTransaction();
         close(database);
